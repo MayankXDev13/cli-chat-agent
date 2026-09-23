f470df8183d2944a6c550574ff0d22680504e0bb
Mayank <vijaymaynk2003@gmail.com>
2026-09-23 21:42:40 +0530

    feat: update ChatOpenAI model configuration for improved agent performance

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ import { ChatOpenAI } from "@langchain/openai";
 import { visitPage, webSearch } from "./tools/index.js";
 
 
+
+const model = new ChatOpenAI({
+  model: "openrouter/free",
+  apiKey: process.env.OPENROUTER_API_KEY,
+  configuration: {
+    baseURL: "https://openrouter.ai/api/v1",
+  },
+});
+
 const agent = createAgent({
-  model:new ChatOpenAI({model:"gpt-4o-mini", streaming:true}),
+  model:model,
   tools:[webSearch, visitPage],
   systemPrompt:"Youa re a helpful assistant. Answer clearly and keep replies short. you also have visit_page for urls , web_search for general search"
 });
